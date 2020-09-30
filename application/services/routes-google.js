8e99cb558852c8334b092e2d18a1556f738699a7
andres reyes <andressantiagore@gmail.com>
2020-09-30 01:14:20 -0500

    finish the function and save the keys

--- application/services/routes-google.js
+++ application/services/routes-google.js
@@ -1,24 +1,39 @@
-#!/usr/bin/env node
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const key = process.env.GOOGLE_API;
+
+
 const routesGoogle = (coordenates) => {
-  const ulr = `maps.googleapis.com/maps/api/directions/json?`
-  const query = `origin=${coordenates.ini.lat},${coordenates.ini.lng}&destination=${coordenates.end.lat},${coordenates.end.lat}&key=${coordenates.lat}`
+  const url = `https://maps.googleapis.com/maps/api/directions/json?` +
+    `origin=${coordenates.ini.lat},${coordenates.ini.lng}` +
+    `&destination=${coordenates.end.lat},${coordenates.end.lng}` +
+    `&key=${key}`
+
+  const requestRoutes = async (url) => {
+    const response = await fetch(url);
+    return response.json();
+  }
+  requestRoutes(url)
+    .then(data => fs.writeFileSync('file.json', JSON.stringify(data)))
+
 }
+
+// const coordenates = {
+//   "ini": {
+//     "lat": 4.6162642,
+//     "lng": -74.1366968,
+//     "address": "Calle 32 # 20 - 68, Bogotá, Colombia"
+//   },
+//   "end": {
+//     "lat": 10.3950646,
+//     "lng": -75.4857664,
+//     "address": "Carrea 62 # 15A - 12, Cartagena, Colombia"
+//   }
+// }
+// routesGoogle(coordenates)
 
 
 module.exports = {
   routesGoogle
 }
-
-const coordenates = {
-  "ini": {
-      "lat": 4.6162642,
-      "lng": -74.1366968,
-      "address": "Calle 32 # 20 - 68, Bogotá, Colombia"
-  },
-  "end": {
-      "lat": 10.3950646,
-      "lng": -75.4857664,
-      "address": "Carrea 62 # 15A - 12, Cartagena, Colombia"
-  }
-}
-console.log(routesGoogle(coordenates))
