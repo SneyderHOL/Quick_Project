2a96a70a8036ec762334b4cfeaf137493246a353
andres reyes <andressantiagore@gmail.com>
2020-10-11 21:14:05 -0500

    fix style

--- controllers/route-tolls.js
+++ controllers/route-tolls.js
@@ -1,15 +1,15 @@
 const requestAll = require('../services/routes-google').requestAll;
-const cost_tolls = require('../services/cost_tolls');
-const { villavicencio, puerto_gaitan, cartagena } = require('../dummies-data');
+// const costTolls = require('../services/cost_tolls');
+// const { villavicencio, puerto_gaitan, cartagena } = require('../dummies-data');
 // /api/tolls
 exports.tollsInRoute = async (req, res, error) => {
   const origin = req.body.points[0];
   const destination = req.body.points[1];
-  const vehicle = req.body.name;
+  // const vehicle = req.body.name;
 
   try {
     const tolls = await requestAll(origin, destination);
-    const total = await cost_tolls.total(tolls, vehicle);
+    // const total = await costTolls.total(tolls, vehicle);
     return res.status(200).send({ total: tolls });
   } catch (e) {
     return res.status(500).send({ error: 'There is a problem, try again' });
