5df5d1e0076ad6264fba198f00b5d1bc9d1acd67
SneyderHOL <1507@holbertonschool.com>
2020-10-13 17:35:26 +0000

    [UPDATE]: fix typo

--- routes/middleware/val-route.js
+++ routes/middleware/val-route.js
@@ -35,6 +35,7 @@ function inputValidationCreation(req, res) {}
  * To further use this value and handle the error
  */
 const validWrongObj = (input) => {
+  console.log(typeof(input));
   return (typeof(input) !== 'object') ? true : false;
 }
 /**
@@ -49,29 +50,29 @@ const badRes = (response, status) => {
   return response.status(status).send({error: 'Bad request'});
 }
 
-export function validateRoutes(req, res, next) {
+exports.validateRoutes = function (req, res, next) {
   if (req.headers['content-type'] !== 'application/json')
     return res.status(400).send('Server requires application/json');
-
+  console.log('1');
   if (req.body === undefined || Object.keys(req.body).length === 0)
     return badRes(res, 400);
-
+  console.log('2');
   if (validWrongObj(req.body.coordinates) || validWrongObj(req.body.vehicle))
     return badRes(res, 422);
-
+  console.log('3');
   if (Object.keys(req.body.coordinates).length === 2)
     return badRes(res, 400);
-
+  console.log('4');
   if (validWrongObj(req.body.coordinates.start) || validWrongObj(req.body.coordinates.dest))
     return badRes(res, 422);
-
+  console.log('5');
   if (!geolib.isValidCoordinate([ req.body.coordinates.start.lat, req.body.coordinates.start.lng ]))
     return badRes(res, 422);
-
+  console.log('6');
   if (!geolib.isValidCoordinate([ req.body.coordinates.dest.lat, req.body.coordinates.dest.lng ]))
     return badRes(res, 422);
-
-  if (Object.keys(req.body.vehicle).length > 1 || validWrongStr(req.body.vehicle.name))
+  console.log('7');
+  if (Object.keys(req.body.vehicle).length >= 1 || validWrongStr(req.body.vehicle.name))
     return badRes(res, 422);
 
   next();
