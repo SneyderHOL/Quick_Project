b8c97acc9628492c918fa969143a7664cfc28fd3
SneyderHOL <1507@holbertonschool.com>
2020-10-13 20:04:06 +0000

    [UPDATE]: add comments

--- services/cost-tolls.js
+++ services/cost-tolls.js
@@ -1,10 +1,14 @@
+/**
+ * categorization of costs according to vehicle and tolls
+ * return: cost of tolls in route
+ */
 exports.total = async (tolls, vehicle) => {
-  const numbertToRome = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII' };
+  const numberToRome = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII' };
   var costs = 0;
   await tolls.forEach((element) => {
     var category = vehicle[0].category;
     var group = category['group' + element.group.toString()];
-    costs += element.costs[numbertToRome[group]];
+    costs += element.costs[numberToRome[group]];
   });
   return costs;
 };
