c58c7d56d2887a8586bf48460851943b9ad7d0bf
SneyderHOL <1507@holbertonschool.com>
2020-10-15 02:01:54 +0000

    [UPDATE]: apply async/await to functions

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -1,66 +1,88 @@
 const Vehicles = require('../models/vehicles');
 
-exports.getVehicles = (req, res) => {
-  Vehicles.findAllVehicles()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Internal error, try again');
-    });
+exports.getVehicles = async (req, res) => {
+  try {
+    const vehicles = { vehicles: await Vehicles.findAllVehicles() };
+    res.status(200).send(vehicles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
-exports.createVehicles = (req, res) => {
-  req.body.map((element) => {
-    Vehicles.create({ ...element })
-      .then((data) => res.status(201).send({ vehicle: data, status: 'Success' }))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Internal error, try again');
-      });
-  });
+exports.createVehicles = async (req, res) => {
+  try {
+    const newVehicle = new Vehicles();
+    newVehicle.name = req.body.name;
+    newVehicle.state = req.body.state;
+    newVehicle.features = req.body.features;
+    newVehicle.axis = req.body.axis;
+    newVehicle.volume = req.body.volume;
+    newVehicle.weight = req.body.weight;
+    newVehicle.fuel_type = req.body.fuel_type;
+    newVehicle.category = req.body.category;
+    await Vehicles.create(newVehicle);
+    res.status(201).send(newVehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 exports.getVehiclesByFeatures = async (req, res) => {
-  Vehicles.findBySpecification(req.params.axis)
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Internal error, try again');
-    });
+  try {
+    var vehicle = await Vehicles.findBySpecification(req.params.axis);
+    if (!vehicle) {
+      res.status(404).send({ error: 'Vehicle Not Found' });
+      return;
+    }
+    res.status(200).send(vehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 exports.findVehicleById = async (req, res) => {
-  Vehicles.findVehicleById(req.params.id)
-    .then((vehicle) => {
-      res.status(200).send(vehicle);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send('Internal error, try again');
-    });
+  try {
+    var vehicle = await Vehicles.findVehicleById(req.params.id);
+    if (!vehicle) {
+      res.status(404).send({ error: 'Vehicle Not Found' });
+      return;
+    }
+    res.status(200).send(vehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 exports.updateVehicles = async (req, res) => {
-  Vehicles.findVehicleById(req.params.id)
-    .then(() => {
-      Vehicles.updateVehicles(req.params.id, { ...req.body })
-        .then((v) => res.status(200).send(v))
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send('Internal error, try again');
-        });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Internal error, try again');
-    });
+  try {
+    let vehicle = null;
+    vehicle = await Vehicles.findVehicleById(req.params.id);
+    if (!vehicle) {
+      res.status(404).send({ error: 'Vehicle Not Found' });
+      return;
+    }
+    const updatedVehicle = await Vehicles.updateVehicles(req.params.id, req.body);
+    return res.status(200).send(updatedVehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 exports.deleteVehicles = async (req, res) => {
-  Vehicles.deleteVehicle(req.params.id)
-    .then(() => res.status(203).send('The vehicle is delete success'))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Internal error, try again');
-    });
+  try {
+    var vehicle = await Vehicles.findVehicleById(req.params.id);
+    if (!vehicle) {
+      res.status(404).send({ error: 'Vehicle Not Found' });
+      return;
+    }
+    res.status(200).send({ status: 'The vehicle was deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
