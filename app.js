d2eac8227b8fe3e3450c59792916053c4bafcb4e
andres reyes <51345583+andresvanegas19@users.noreply.github.com>
2020-10-15 19:34:20 -0500

    modificated the structure of app js (#10)
    * Update app.js
    
    * Delete launch.json
    
    * Delete www
    
    * Update app.js
    
    * Update app.js
    
    * Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
+/**
+ * Module dependencies.
+ */
+const debug = require('debug')('application:server');
+const http = require('http');
 const swaggerUI = require('swagger-ui-express');
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const yaml = require('yamljs');
 require('dotenv').config();
+const cors = require('cors');
 
 // will use in the future
 // const swaggerJSDocs = require('swagger-jsdoc');
 const swaggerJS = yaml.load('./documentation.yaml');
 
 const app = express();
+app.use(cors());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJS));
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(function (err, req, res, next) {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    // Handle the error here
+    console.error('Bad JSON');
+    res.status(400).send({ error: 'JSON parse error' });
+  }
+  // Pass the error to the next middleware if it wasn't a JSON parse error
+  next(err);
+});
+
 app.use(express.urlencoded({ extended: false }));
 const mongoDB = process.env.URL_DB;
 
@@ -45,4 +63,71 @@ app.use((req, res, next) => {
   next();
 });
 
-module.exports = app;
+/**
+ * Get port from environment and store in Express.
+ */
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort (val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) return val;
+
+  if (port >= 0) return port;
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError (error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening () {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+module.exports = { app, mongoose };
