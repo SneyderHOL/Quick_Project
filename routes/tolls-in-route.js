7532334dbb7fe7e72881d39fa3b2bd9817ebf7fe
andres reyes <andressantiagore@gmail.com>
2020-10-17 04:30:12 -0500

    delete middleware

--- routes/tolls-in-route.js
+++ routes/tolls-in-route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controllerTollsInRoute = require('../controllers/route-tolls');
 const middleware = require('./middleware/val-route').validateRoutes;
 
-router.post('/', middleware, controllerTollsInRoute.totalCosts);
+router.post('/', controllerTollsInRoute.totalCosts);
 
 module.exports = router;
